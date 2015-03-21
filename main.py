896afeab02c59c285299fd0324f10511c6785371
lpo1234 <lpo1017@frontiernet.net>
2015-03-21 02:05:44 -0400

    Changed main.py so it would link to camera on robot. Fixed horizontal image flip.

--- main.py
+++ main.py
@@ -81,14 +81,15 @@ class robotGUI():
         #Create video feed 
         self.videoFrame = tk.Label(self.main)
         self.videoFrame.pack()
-        self.videoStream = cv2.VideoCapture(0)
+        self.videoStream = cv2.VideoCapture(1)  #1 is for camera control
+                                                #0 is for local camera
         self.startVideoFeed()
 
         tk.mainloop()
 
     def startVideoFeed(self):
         _, frame = self.videoStream.read()
-        frame = cv2.flip(frame, 1)
+        #frame = cv2.flip(frame, 1)
         cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
         img = Image.fromarray(cv2image)
         imgtk = ImageTk.PhotoImage(image=img)
